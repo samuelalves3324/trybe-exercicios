add11cb1ab429bf95e7a46db6eb4cb9fa4781347
Samuel Alves <samucahhh@gmail.com>
2022-09-26 18:13:25 -0300

    Adiciona exercicios

---
+++ fundamentos/secao-04-introducao-a-javascript-e-logica-de-programacao/dia-03-java-script-logica-de-programacao-e-algoritmos/exercicios-bonus.js
@@ -0,0 +1,30 @@
+let numeroQuadrado = 8;
+let asteriscoQuadrado = '';
+
+
+for (let index = 1; index <= numeroQuadrado; index += 1) {
+  asteriscoQuadrado += "*";
+}
+for (let index = 1; index <= numeroQuadrado; index += 1) {
+  console.log(asteriscoQuadrado);
+}
+
+console.log('');
+
+let numeroTriangulo = 6;
+let asteriscoTriangulo = '';
+
+for (let index = 1; index <= numeroTriangulo; index += 1) {
+  asteriscoTriangulo += "*";
+  console.log(asteriscoTriangulo);
+}
+
+console.log('');
+
+let numeroTrianguloInvertido = 6;
+let asteriscoTrianguloInvertido = '';
+
+for (let index = 1; index <= numeroTrianguloInvertido; index += 1) {
+  asteriscoTrianguloInvertido += "*";
+  console.log(asteriscoTrianguloInvertido);
+}
