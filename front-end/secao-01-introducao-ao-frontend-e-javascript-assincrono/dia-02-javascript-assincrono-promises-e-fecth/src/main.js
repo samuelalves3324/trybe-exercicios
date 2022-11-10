11d2ea36b1ffcb4e7902c87889acc66eb6820062
Samuel Alves <samucahhh@gmail.com>
2022-11-10 00:04:31 -0300

    add style in project

--- front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fecth/src/main.js
+++ front-end/secao-01-introducao-ao-frontend-e-javascript-assincrono/dia-02-javascript-assincrono-promises-e-fecth/src/main.js
@@ -3,14 +3,16 @@ import "./style.css"
 const img = document.getElementById('img');
 const text = document.getElementById('text');
 const button = document.getElementById('button');
+const trueName = document.getElementById('true-name');
 
 button.addEventListener('click', () => {
   const randomNumber = Math.round(Math.random() * 731)
   fetch(`https://akabab.github.io/superhero-api/api/id/${randomNumber}.json`)
   .then(response => response.json())
   .then(data => {
-  img.src = data.images.lg;
+  img.src = data.images.md;
   text.innerHTML = data.name;
+  trueName.innerHTML = `Nome verdadeiro: ${data.biography.fullName}`;
   })
   .catch(error => alert(`Número sorteado inválido`))
 })
